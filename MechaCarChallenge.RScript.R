87f6e273e17690960220f256b8ee0a3ffc7ccf7e
Holly Smith <97558998+HollyESmith@users.noreply.github.com>
2022-06-02 14:28:30 -0500

    Final script

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,5 +1,8 @@
+# DELIVERABLE 1
+
 # 3. Use the library() function to load the dplyr package
 library(dplyr)
+library(tidyverse)
 
 # 4. Import and read in the MechaCar_mpg.csv file as a dataframe
 MechaCarMpg <- read.csv(file = "MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
@@ -10,4 +13,38 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # 6. Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCarMpg))
+
+# DELIVERABLE 2 -----------------------------
+
+# 2. Import and read in the Suspension_Coil.csv file as a table
+MechaCarCoil <- read.csv(file = "Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+
+# 3. Create a total_summary dataframe using the summarize() function to get the 
+# mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+total_summary <- MechaCarCoil %>% summarize(Mean=mean(PSI),
+                                            Median=median(PSI),
+                                            Variance=var(PSI),
+                                            StdDev=sd(PSI))
+
+# 4. Create a lot_summary dataframe using the group_by() and the summarize() functions to 
+# group each manufacturing lot by the mean, median, variance, and standard deviation of 
+# the suspension coil’s PSI column
+lot_summary <- MechaCarCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),
+                                                                          Median=median(PSI),
+                                                                          Variance=var(PSI),
+                                                                          StdDev=sd(PSI),
+                                                                          .groups='keep')
+
+# DELIVERABLE 3 -----------------------------
+# 1. Use the t.test() function to determine if the PSI across all manufacturing lots is statistically
+# different from the population mean of 1,500 pounds per square inch.
+t.test(MechaCarCoil$PSI,mu=1500)
+
+
+# 2. Write 3 more RScripts using the t.test() function and its subset() argument to determine if 
+# the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(subset(MechaCarCoil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+t.test(subset(MechaCarCoil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)    
+t.test(subset(MechaCarCoil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+       
 
